7fdcd58bf6d5e5488e00f473cd66abdf33338423
ilario mazzieri <ilario.mazzieri@polimi.it>
2021-09-12 21:56:16 +0200

    Change mechanical parameters + boundary conditions

--- MAKE_MECH_PROP_CASE_060.f90
+++ MAKE_MECH_PROP_CASE_060.f90
@@ -49,18 +49,64 @@
               qs = 25
               qp = 44
               gamma = (3.1415*(2.d0/3.d0))/qs   
+
+              !left
+              if(dabs(xs - 628957.563d0) .le. 5000.d0) then 
+                VS = 582.d0
+                VP  = 1000.d0
+                rho = 1200.d0           
+                lambda = rho * (VP**2 - 2*VS**2)            
+                mu = rho * VS**2  
+                qs = 25
+                qp = 44
+                gamma = (3.1415*(2.d0/3.d0))/qs   
+              !right
+              elseif(dabs(xs - 739448.625d0) .le. 5000.d0) then 
+                VS = 582.d0
+                VP  = 1000.d0
+                rho = 1200.d0           
+                lambda = rho * (VP**2 - 2*VS**2)            
+                mu = rho * VS**2  
+                qs = 25
+                qp = 44
+                gamma = (3.1415*(2.d0/3.d0))/qs   
+              !up
+              elseif(dabs(ys - 9354624.d0) .le. 5000.d0) then 
+                VS = 582.d0
+                VP  = 1000.d0
+                rho = 1200.d0           
+                lambda = rho * (VP**2 - 2*VS**2)            
+                mu = rho * VS**2  
+                qs = 25
+                qp = 44
+                gamma = (3.1415*(2.d0/3.d0))/qs   
+              !down
+              elseif(dabs(ys - 9262575.d0) .le. 5000.d0) then 
+                VS = 582.d0
+                VP  = 1000.d0
+                rho = 1200.d0           
+                lambda = rho * (VP**2 - 2*VS**2)            
+                mu = rho * VS**2  
+                qs = 25
+                qp = 44
+                gamma = (3.1415*(2.d0/3.d0))/qs   
+             endif      
        else  
               VS = 2300.d0;
               VP = 4100.d0;
-              rho = 200.d0;
+              rho = 2000.d0;
               lambda = rho * (VP**2 - 2*VS**2)            
               mu = rho * VS**2 
               qs = 150
               qp = 283            
               gamma = (3.1415*(2.d0/3.d0))/qs   
       endif           
-               
-              
+
+      !Left Top    629160.875000  9354624.000000   -32.000000
+      !Left Down   628957.563000  9262946.000000   443.000000
+
+      !Right Top   739448.625000  9354298.000000   -28.000000
+      !Right Down  739071.438000  9262575.000000   406.000000          
      
  
      end subroutine MAKE_MECH_PROP_CASE_060        
